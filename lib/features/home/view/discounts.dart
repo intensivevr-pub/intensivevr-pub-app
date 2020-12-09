4d11cca1a1c1555ff623c1d97aac79a8ca887f2c
Fermento99 <pk.pawlak99@gmail.com>
2020-12-10 00:10:29 +0100

    discounts in SingleChildScrollView

--- lib/features/home/view/discounts.dart
+++ lib/features/home/view/discounts.dart
@@ -8,37 +8,41 @@ class Discounts extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 250,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.0),
-        color: Colors.grey,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Text(
-              "Aktualne promocje:",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Container(
+          decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.0),
+          color: Colors.grey,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  "Aktualne promocje:",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
               ),
-            ),
+              Container(
+                height: 200,
+                child: Row(
+                  children: [
+                    DiscountPanel(color: Colors.teal, product: "Perła", discount: "-20%",),
+                    DiscountPanel(color: Colors.teal, product: "Perła", discount: "-20%",),
+                    DiscountPanel(color: Colors.teal, product: "Perła", discount: "-20%",),
+                    DiscountPanel(color: Colors.teal, product: "Perła", discount: "-20%",),
+                    DiscountPanel(color: Colors.teal, product: "Perła", discount: "-20%",),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Container(
-            height: 200,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                DiscountPanel(color: Colors.teal, product: "Perła", discount: "-20%",),
-                DiscountPanel(color: Colors.teal, product: "Perła", discount: "-20%",),
-                DiscountPanel(color: Colors.teal, product: "Perła", discount: "-20%",),
-                DiscountPanel(color: Colors.teal, product: "Perła", discount: "-20%",),
-                DiscountPanel(color: Colors.teal, product: "Perła", discount: "-20%",),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
